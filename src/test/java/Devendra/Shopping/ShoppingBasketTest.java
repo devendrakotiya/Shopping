35baa8367952aee2a107f6482af7a9bf1a036750
devendrakotiya <devendrakotiya01@gmail.com>
2025-03-10 22:35:53 +0530

    few testcases added

--- src/test/java/Devendra/Shopping/ShoppingBasketTest.java
+++ src/test/java/Devendra/Shopping/ShoppingBasketTest.java
@@ -6,45 +6,74 @@ import org.testng.annotations.Test;
 
 public class ShoppingBasketTest {
 	
-        // Test case 1:
-		@Test
-		public void BasicExampleWithMultipleItems() {
-        List<String> basket1 = List.of("Apple", "Apple", "Banana", "Melon", "Melon", "Lime", "Lime", "Lime");
-        System.out.println("Test 1 - Total cost: " + ShoppingBasket.calculateTotalCost(basket1) + "p"); // Expected: 185p
-		}
-		
-        // Test case 2
-		@Test
-		public void BasketWithOnlyApples() {
-        List<String> basket2 = List.of("Apple", "Apple", "Apple");
-        System.out.println("Test 2 - Total cost: " + ShoppingBasket.calculateTotalCost(basket2) + "p"); // Expected: 105p
-		}
-		
-        // Test case 3
-		@Test
-		public void BasketWithOnlyBananas() {
-        List<String> basket3 = List.of("Banana", "Banana");
-        System.out.println("Test 3 - Total cost: " + ShoppingBasket.calculateTotalCost(basket3) + "p"); // Expected: 40p
-		}
-		
-        // Test case 4:
-		@Test
-		public void BasketWithOnlyMelons() {
-        List<String> basket4 = List.of("Melon", "Melon", "Melon");
-        System.out.println("Test 4 - Total cost: " + ShoppingBasket.calculateTotalCost(basket4) + "p"); // Expected: 50p
-		}
-		
-        // Test case 5:
-		@Test
-		public void BasketWithOnlyLimes() {
-        List<String> basket5 = List.of("Lime", "Lime", "Lime", "Lime", "Lime");
-        System.out.println("Test 5 - Total cost: " + ShoppingBasket.calculateTotalCost(basket5) + "p"); // Expected: 60p
-		}
-		
-        // Test case 6:
-		@Test
-		public void EmptyBasket() {
-        List<String> basket6 = List.of();
-        System.out.println("Test 6 - Total cost: " + ShoppingBasket.calculateTotalCost(basket6) + "p"); // Expected: 0p
-		}
+	@Test
+	public void A_Basic_example_with_multiple_items() {
+	List<String> basket1 = List.of("Apple", "Apple", "Banana", "Melon", "Melon", "Lime", "Lime", "Lime");
+    System.out.println("Test 1 - Total cost: " + ShoppingBasket.calculateTotalCost(basket1) + "p"); 
+	}
+	
+    
+	@Test
+	public void Only_apples_in_the_basket() {
+    List<String> basket2 = List.of("Apple", "Apple", "Apple");
+    System.out.println("Test 2 - Total cost: " + ShoppingBasket.calculateTotalCost(basket2) + "p"); 
+	}
+	
+    
+	@Test
+	public void Only_bananas_in_the_basket() {
+    List<String> basket3 = List.of("Banana", "Banana");
+    System.out.println("Test 3 - Total cost: " + ShoppingBasket.calculateTotalCost(basket3) + "p"); 
+	}
+	
+    
+	@Test
+	public void Basket_with_melons() {
+    List<String> basket4 = List.of("Melon", "Melon", "Melon");
+    System.out.println("Test 4 - Total cost: " + ShoppingBasket.calculateTotalCost(basket4) + "p"); 
+	}
+	
+   
+	@Test
+	public void Basket_with_limes() {
+    List<String> basket5 = List.of("Lime", "Lime", "Lime", "Lime", "Lime");
+    System.out.println("Test 5 - Total cost: " + ShoppingBasket.calculateTotalCost(basket5) + "p"); 
+	}
+	
+    
+    @Test
+    public void An_empty_basket() {
+	List<String> basket6 = List.of();
+    System.out.println("Test 6 - Total cost: " + ShoppingBasket.calculateTotalCost(basket6) + "p"); 
+    }
+    
+  
+    @Test
+    public void A_basket_with_odd_and_even_counts_for_melons() {
+    List<String> basket7 = List.of("Melon", "Melon", "Melon", "Melon", "Melon", "Melon");
+    System.out.println("Test 7 - Total cost: " + ShoppingBasket.calculateTotalCost(basket7) + "p"); 
+    }
+    
+    
+    @Test
+    public void Basket_with_only_limes() {
+    List<String> basket8 = List.of("Lime", "Lime", "Lime", "Lime", "Lime", "Lime", "Lime", "Lime", "Lime");
+    System.out.println("Test 8 - Total cost: " + ShoppingBasket.calculateTotalCost(basket8) + "p");
+    }
+    
+    
+    @Test
+    public void Basket_with_just_one_lime() {
+    List<String> basket9 = List.of("Lime");
+    System.out.println("Test 9 - Total cost: " + ShoppingBasket.calculateTotalCost(basket9) + "p"); 
+ 
+    }
+    
+    
+    @Test
+    public void Basket_with_only_one_melon() {
+    List<String> basket10 = List.of("Melon");
+    System.out.println("Test 10 - Total cost: " + ShoppingBasket.calculateTotalCost(basket10) + "p"); 
+    }
+    
 }
